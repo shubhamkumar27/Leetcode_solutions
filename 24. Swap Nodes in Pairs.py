d90f7e33049e7f0b7b7cc131a95f4c945f762416
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-24 22:15:24 +0530

    Question 24

--- 24. Swap Nodes in Pairs.py
+++ 24. Swap Nodes in Pairs.py
@@ -6,3 +6,17 @@ Example:
 Given 1->2->3->4, you should return the list as 2->1->4->3.
 '''
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        if head and head.next:
+            temp = head.next
+            head.next = self.swapPairs(temp.next)
+            temp.next = head
+            return temp
+        return head
