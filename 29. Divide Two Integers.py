b5879cad442628689839449d670f623a111b06ee
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-26 22:15:47 +0530

    Question 29

--- 29. Divide Two Integers.py
+++ 29. Divide Two Integers.py
@@ -19,3 +19,20 @@ Assume we are dealing with an environment which could only store integers within
 signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your 
 function returns 231 − 1 when the division result overflows.
 '''
+
+class Solution:
+    def divide(self, dividend, divisor):
+        if dividend<0 and divisor<0 or dividend>0 and divisor>0:
+            ans = 0
+        else:
+            ans = 1
+        count = 0
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        while(abs(dividend)>=abs(divisor)):
+            # print(abs(dividend),abs(divisor))
+            dividend-=divisor
+            count += 1
+        if ans:
+            return max(2**31-1,count - count - count, -2**31)
+        else: return max(2**31-1,count, -2**31)
