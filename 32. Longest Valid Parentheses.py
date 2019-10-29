dae352c1ce0632481951f02a36c99f891f2cbf78
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-29 15:53:01 +0530

    Question 32

--- 32. Longest Valid Parentheses.py
+++ 32. Longest Valid Parentheses.py
@@ -13,3 +13,25 @@ Output: 4
 Explanation: The longest valid parentheses substring is "()()"
 '''
 
+class Solution:
+    def longestValidParentheses(self, s):
+        if (len(s)==0 or s==None):
+            return 0
+        l = len(s)
+        stack = [0]
+        for i in range(1,l):
+            if len(stack)==0:
+                stack.append(i)
+            elif s[i]==')' and s[stack[-1]]=='(':
+                stack.pop()
+            else:
+                stack.append(i)
+        print(stack)
+        if stack==[]:
+            return l
+        maxm = max(stack[0],l-stack[-1]-1)
+        for i in range(1, len(stack)):
+            sub = stack[i]-stack[i-1]-1
+            if sub > maxm:
+                maxm = sub
+        return maxm
