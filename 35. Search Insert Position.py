993b213fa1328a91b7218b247a8e448436e26377
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-10-30 09:56:07 +0530

    Question 35

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -19,3 +19,24 @@ Example 4:
 Input: [1,3,5,6], 0
 Output: 0
 '''
+
+class Solution:
+    def searchInsert(self, nums, target):
+        if nums==[]:
+            return 0
+        l = len(nums)
+        i = 0
+        j = l-1
+        if target>nums[j]:
+            return l
+        elif target<nums[i]:
+            return 0
+        while(i<=j):
+            mid = i + (j-i)//2
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>target:
+                j = mid-1
+            if nums[mid]<target:
+                i = mid+1
+        return i
