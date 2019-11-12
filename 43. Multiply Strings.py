5ac7d59a5c7aa16716900e36be0efe28fbf67803
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-11-12 11:11:43 +0530

    Question 43

--- 43. Multiply Strings.py
+++ 43. Multiply Strings.py
@@ -16,3 +16,24 @@ Both num1 and num2 contain only digits 0-9.
 Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 You must not use any built-in BigInteger library or convert the inputs to integer directly.
 '''
+
+class Solution:
+    def multiply(self, num1, num2):
+        m = len(num1)
+        n = len(num2)
+        arr = [0]*(m+n)
+        carry = 0
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                arr[i+j+1] += (ord(num1[i])-ord('0'))*(ord(num2[j])-ord('0'))
+        
+        for i in range(len(arr)-1,-1,-1):
+            arr[i] += carry
+            carry = arr[i]//10
+            arr[i] = str(arr[i]%10)
+        
+        for i in range(len(arr)):
+            if arr[i]!= '0' or i==len(arr)-1:
+                break
+                  
+        return ''.join(arr[i:])
