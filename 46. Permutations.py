1f5daf29f25ab856477e3b67a74c3aca6dbc35b1
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-11-13 14:04:01 +0530

    Question 46

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -13,3 +13,22 @@ Output:
   [3,2,1]
 ]
 '''
+
+class Solution:
+    def permute(self, nums):
+        if len(nums)==0:
+            return []
+        result = []
+        return self.recursive(nums, 0 ,result)
+    
+    def recursive(self,nums,start,result):
+        if start>=len(nums)-1:
+            result.append(nums[:])
+            return result
+            
+        for i in range(start,len(nums)):
+            nums[start],nums[i]=nums[i],nums[start]
+            result = self.recursive(nums, start+1, result)
+            nums[start],nums[i]=nums[i],nums[start]
+        
+        return result
