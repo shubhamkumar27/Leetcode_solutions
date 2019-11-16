f86efcc475c0e18d4abb83b95e9903e5f6aafb3f
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-11-16 21:44:22 +0530

    Question 47

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -18,3 +18,21 @@ class Solution:
         result = []
         nums.sort()
         return self.recursive(nums,0,result)
+    
+        
+    def recursive(self, nums, start, result):
+        myset = set()
+        if start>=len(nums)-1:
+            #print(start,nums)
+            result.append(nums[:])
+            return result
+        
+        for i in range(start,len(nums)):
+            #print(start,i)
+            if nums[i] not in myset:
+                myset.add(nums[i])
+                nums[start], nums[i] = nums[i], nums[start]
+                self.recursive(nums,start+1,result)
+                nums[start], nums[i] = nums[i], nums[start]
+            
+        return result
