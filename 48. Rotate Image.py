6645c8dfc7a5edffbd41d1e286e4569700380aec
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-11-16 22:38:02 +0530

    Question 48

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -38,3 +38,19 @@ rotate the input matrix in-place such that it becomes:
   [16, 7,10,11]
 ]
 '''
+
+class Solution:
+    def rotate(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l = len(matrix)
+        if l==1 or l==0:
+            return
+        for i in range(l//2):
+            for j in range(i,l-1):
+                matrix[i][j],matrix[j][l-1] = matrix[j][l-1], matrix[i][j]
+                matrix[i][j],matrix[l-1][l-1-j+i] = matrix[l-1][l-1-j+i], matrix[i][j]
+                matrix[i][j],matrix[l-1-j+i][i] = matrix[l-1-j+i][i], matrix[i][j]
+            l-=1
+        return
