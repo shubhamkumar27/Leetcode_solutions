36e7ee97d06bf9cf97a7b842b9a19eebb43f803a
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-11-17 14:03:07 +0530

    Question 49

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -15,3 +15,19 @@ All inputs will be in lowercase.
 The order of your output does not matter.
 '''
 
+class Solution:
+    def groupAnagrams(self, strs):
+        l = len(strs)
+        has = {}
+        for i in range(l):
+            l = list(strs[i])
+            l.sort()
+            string = ''.join(l)
+            if string in has:
+                has[string] += [strs[i]]
+            else:
+                has[string] = [strs[i]]
+        res = []
+        for key in has:
+            res.append(has[key])
+        return res
