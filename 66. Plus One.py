71e0a01286550009141dd33a45224521c97da482
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-12-07 09:44:52 +0530

    Question 66

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -13,3 +13,13 @@ Input: [4,3,2,1]
 Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 '''
+
+
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        return [1]+[0]*len(digits)
