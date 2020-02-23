875ed43fa9a998755049c0e18ad86a82464762ba
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-02-23 20:15:16 +0530

    Question 64

--- 64. Minimum Path Sum.py
+++ 64. Minimum Path Sum.py
@@ -14,3 +14,20 @@ Input:
 Output: 7
 Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 '''
+class Solution:
+    def minPathSum(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        dp_grid = grid[:][:]
+        
+        for i in range(1,m):
+            dp_grid[i][0] += grid[i-1][0]
+
+        for i in range(1,n):
+            dp_grid[0][i] += dp_grid[0][i-1]
+
+        for i in range(1,m):
+            for j in range(1,n):
+                dp_grid[i][j] += min(dp_grid[i-1][j],dp_grid[i][j-1])
+
+        return dp_grid[-1][-1]
