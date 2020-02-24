528e91d29a949dc70747268c159b69194a7de547
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-02-25 00:08:20 +0530

    Question 63

--- 63. Unique Paths II.py
+++ 63. Unique Paths II.py
@@ -1,18 +1,15 @@
 '''
 A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
-
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the 
+bottom-right corner of the grid (marked 'Finish' in the diagram below).
 
 Now consider if some obstacles are added to the grids. How many unique paths would there be?
-
-
 
 An obstacle and empty space is marked as 1 and 0 respectively in the grid.
 
 Note: m and n will be at most 100.
 
 Example 1:
-
 Input:
 [
   [0,0,0],
@@ -20,6 +17,7 @@ Input:
   [0,0,0]
 ]
 Output: 2
+
 Explanation:
 There is one obstacle in the middle of the 3x3 grid above.
 There are two ways to reach the bottom-right corner:
