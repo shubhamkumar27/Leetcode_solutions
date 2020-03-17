3f409c662cda487cd6ea2beda229a5844bfea18c
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-03-17 22:32:11 +0530

    Question 17

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -12,3 +12,19 @@ Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 '''
 
+def letterCombinations(digits):
+    digits = str(digits)
+    phone = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+    res = helper(digits,phone)
+    # result = ' '.join(res)
+    return res
+
+def helper(digits,phone):
+    if len(digits)==0:
+        return []
+    if len(digits)==1:
+        return list(phone[digits])
+    back = helper(digits[1:],phone)
+    front = list(phone[digits[0]])
+    res = [f+b for f in front  for b in back]
+    return
