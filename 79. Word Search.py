bbddf5224c30a7815774e176903deac22c0e1822
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-17 00:27:40 +0530

    Question 79

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -25,3 +25,37 @@ board and word consists only of lowercase and uppercase English letters.
 1 <= word.length <= 10^3
 '''
 
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                visited = set()
+                if finder(i,j,board, word, visited):
+                    return True
+        return False
+        
+
+def finder(row, column, grid, word, visited):
+    if word=='':
+        return True
+    # print(visited, row, column, word)
+    if grid[row][column]==word[0] and (row, column) not in visited:
+        visited.add((row, column))
+        if len(grid)==1 and len(grid[0])==1 and len(word)==1:
+            return True    
+        if column+1<len(grid[0]):
+            if finder(row, column+1, grid, word[1:], visited):
+                return True
+        if row+1<len(grid):
+            if finder(row+1, column, grid, word[1:], visited):
+                return True
+        if column-1>=0:
+            if finder(row, column-1, grid, word[1:], visited):
+                return True
+        if row-1>=0:
+            if finder(row-1, column, grid, word[1:], visited):
+                return True
+
+        visited.remove((row, column))
+            
+    return False
