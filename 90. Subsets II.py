790e115a40747ddcb666adbaf29ee24621be54f8
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-18 23:28:12 +0530

    Question 90

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -17,3 +17,16 @@ Output:
 ]
 '''
 
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        recursion(nums, 0, [], result)
+        return result
+               
+def recursion(nums, index, path, result):
+    result.append(path)
+    for i in range(index, len(nums)):
+        if i>index and nums[i]==nums[i-1]:
+            continue
+        recursion(nums, i+1, path+[nums[i]], result)
