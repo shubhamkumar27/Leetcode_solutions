09a784c18d5b7ff7d4e4c9f11c80ef099c44e022
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-18 23:39:37 +0530

    Question 51

--- 51. N-Queens.py
+++ 51. N-Queens.py
@@ -1,11 +1,11 @@
 '''
-The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
-
-
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens 
+attack each other.
 
 Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' 
+both indicate a queen and an empty space respectively.
 
 Example:
 
@@ -24,3 +24,44 @@ Output: [
 Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
 '''
 
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+        queensColumn = [-1]*n
+        placeQueens(0, queensColumn,n, result)
+        return result
+
+def isValidPos(queensColumn, col, row):
+    valid = True
+    for i in range(row):
+
+        if queensColumn[i]==col:
+            valid = False
+
+        if abs(col-queensColumn[i])==abs(row-i):
+            valid = False
+
+    return valid
+
+def addResult(queensColumn,n, result):
+    res = []
+    for i in range(n):
+        cur = ''
+        for j in range(n):
+            if j==queensColumn[i]:
+                cur+='Q'
+            else:
+                cur+='.'
+        res.append(cur)
+    result.append(res)
+
+
+def placeQueens(row, queensColumn, n, result):
+    if row==n:
+        addResult(queensColumn, n, result)
+        return
+    
+    for i in range(n):
+        if isValidPos(queensColumn, i, row):
+            queensColumn[row] = i
+            placeQueens(row+1, queensColumn, n, result)
