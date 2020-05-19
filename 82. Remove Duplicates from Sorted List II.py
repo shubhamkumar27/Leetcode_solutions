9d885a29dcf62373858cfed24d9f0a9c461c1c02
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-19 23:55:50 +0530

    Question 82

--- 82. Remove Duplicates from Sorted List II.py
+++ 82. Remove Duplicates from Sorted List II.py
@@ -13,3 +13,43 @@ Input: 1->1->1->2->3
 Output: 2->3
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head==None or head.next==None:
+            return head
+        
+        if head.next.val == head.val:
+            temp = head
+            head = head.next
+            while(head):
+                if head.val!=temp.val:
+                    if head.next==None:
+                        break
+                    elif head.val!=head.next.val:
+                        break
+                temp = temp.next
+                head = head.next
+            
+            if head==None:
+                return head
+            
+        pointer = head
+        prev = head
+        temp = head.next
+        while(temp):
+            if prev.val!=temp.val:
+                if temp.next==None or temp.next.val!=temp.val:
+                    pointer.next = temp
+                    pointer = temp
+            prev = prev.next
+            temp = temp.next
+        pointer.next = temp
+        return head
+    
---
+++ 83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,34 @@
+'''
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head==None or head.next==None:
+            return head
+        pre = head
+        cur = head.next
+        while(cur):
+            if pre.val==cur.val:
+                pre.next = cur.next
+                cur = cur.next
+            else:
+                pre = pre.next
+                cur = cur.next
+        return head
