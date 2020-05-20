a0c778fda67f6adb8beb54bbdeb76cc8c48f4c18
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-20 23:48:17 +0530

    Question 72

--- 72. Edit Distance.py
+++ 72. Edit Distance.py
@@ -26,3 +26,26 @@ exention -> exection (replace 'n' with 'c')
 exection -> execution (insert 'u')
 '''
 
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[-1 for i in range(len(word2))] for j in range(len(word1))]
+        i = len(word1)-1
+        j = len(word2)-1
+        return recursion(word1, word2, i, j, dp)
+        
+        
+def recursion(w1, w2, i, j, dp):
+    if i<0 or j<0:
+        return abs(i-j)
+
+    if dp[i][j]!=-1:
+        return dp[i][j]
+    
+    if w1[i]==w2[j]:
+        return recursion(w1, w2, i-1, j-1, dp)
+    a = 1+recursion(w1, w2, i, j-1, dp)
+    b = 1+recursion(w1, w2, i-1, j, dp)
+    c = 1+recursion(w1, w2, i-1, j-1, dp)
+    dp[i][j] = min(a,b,c)
+    
+    return min(a,b,c)
