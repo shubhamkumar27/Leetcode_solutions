1650b334d039ba7ae368eb95bcb2d426b905f64f
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-20 23:51:24 +0530

    Question 74

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -25,3 +25,12 @@ target = 13
 Output: false
 '''
 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            if len(matrix[i])>0 and matrix[i][0]<=target<=matrix[i][-1]:
+                for j in matrix[i]:
+                    if j==target:
+                        return True
+        return False
+        
