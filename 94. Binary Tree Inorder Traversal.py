1085ad386fbb58fd58270acf5bc2269516613b0c
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-21 23:45:13 +0530

    Question 94

--- 94. Binary Tree Inorder Traversal.py
+++ 94. Binary Tree Inorder Traversal.py
@@ -14,3 +14,23 @@ Output: [1,3,2]
 Follow up: Recursive solution is trivial, could you do it iteratively?
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        inOrder = []
+        inOrderTraversal(root, inOrder)
+        return inOrder
+        
+def inOrderTraversal(root, inOrder):
+    if root==None:
+        return
+    inOrderTraversal(root.left, inOrder)
+    inOrder.append(root.val)
+    inOrderTraversal(root.right, inOrder)
