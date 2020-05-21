2652267d85c47e1f554b083ba40976b15f957b4c
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-21 23:48:24 +0530

    Question 98

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -29,3 +29,29 @@ Output: false
 Explanation: The root node's value is 5 but its right child's value is 4.
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        return DFSverify(root, 2**31-1, -2**31)
+
+    
+def DFSverify(root, less, greater):
+    if root==None:
+        return True
+    
+    if root.val>=less:
+        return False
+    
+    if root.val<=greater:
+        return False
+    
+    left = DFSverify(root.left, min(root.val, less), greater)
+    right = DFSverify(root.right, less, max(root.val, greater))
+    
+    return left and right
