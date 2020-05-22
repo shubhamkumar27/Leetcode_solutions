51000b0ab2ec45771ded98a5d78a9a421287dd49
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-22 23:34:27 +0530

    Question 86

--- 86. Partition List.py
+++ 86. Partition List.py
@@ -9,3 +9,39 @@ Input: head = 1->4->3->2->5->2, x = 3
 Output: 1->2->2->4->3->5
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        if head==None or head.next==None:
+            return head
+        flag = False
+        prev = None
+        temp = head
+        if head.val<x:
+            flag = True
+            prev = head
+        while(temp.next):
+            if temp.next.val<x:
+                current = temp.next
+                temp.next = temp.next.next
+                if flag:
+                    current.next = prev.next
+                    prev.next = current
+                    prev = prev.next
+                else:
+                    current.next = head
+                    head = current
+                    prev = head
+                    flag = True
+                if temp.next==current:
+                    temp = temp.next
+            else:
+                temp = temp.next
+        return head
+            
