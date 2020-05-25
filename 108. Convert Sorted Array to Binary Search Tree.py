6cfcd3d65cca164eb9fd77325499227347258fdd
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-25 23:46:23 +0530

    Question 108

--- 108. Convert Sorted Array to Binary Search Tree.py
+++ 108. Convert Sorted Array to Binary Search Tree.py
@@ -23,17 +23,47 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 #         self.left = left
 #         self.right = right
 
-class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        if len(nums)==0:
-            return None
+# class Solution:
+#     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+#         if len(nums)==0:
+#             return None
         
-        mid = len(nums)//2
+#         mid = len(nums)//2
         
-        node = TreeNode(nums[mid])
+#         node = TreeNode(nums[mid])
         
-        node.left = self.sortedArrayToBST(nums[:mid])
-        node.right = self.sortedArrayToBST(nums[mid+1:])
+#         node.left = self.sortedArrayToBST(nums[:mid])
+#         node.right = self.sortedArrayToBST(nums[mid+1:])
         
-        return node
+#         return node
+
+test = int(input())
+for _ in range(test):
+    n = int(input())
+    seq = list(map(int, input().split()))
+    ingred = set()
+    freq = set()
+    i = 0
+    flag = True
+    while(i<n):
+        start = seq[i]
+        f = 0
+        # print(start, ingred,freq, i, f)
+        if start in ingred:
+            print('NO')
+            break
+        while(i<n and seq[i]==start):
+            f+=1
+            i+=1
+        # print(start, ingred,freq, i, f)
+        if f in freq:
+            print('NO')
+            break
+        else:
+            ingred.add(start)
+            freq.add(f)
+            i-=1
+        i+=1
+    print('YES')
         
+  
