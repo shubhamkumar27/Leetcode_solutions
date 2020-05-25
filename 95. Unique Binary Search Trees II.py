3c79158c3a58685b945376f9d1378ace295b688c
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-25 23:51:59 +0530

    Question 95

--- 95. Unique Binary Search Trees II.py
+++ 95. Unique Binary Search Trees II.py
@@ -22,3 +22,38 @@ The above output corresponds to the 5 unique BST's shown below:
    2     1         2                 3
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n==0:
+            return None
+        lis = [i for i in range(1,n+1)]
+        return treeHelper(lis)
+        
+        
+def treeHelper(lis):
+    if len(lis)==0:
+        return [None]
+    
+    val = []
+    for i in range(len(lis)):
+        left = treeHelper(lis[:i])
+        if i<len(lis)-1:
+            right = treeHelper(lis[i+1:])
+        else:
+            right = [None]
+        for l in range(len(left)):
+            for r in range(len(right)):
+                root = TreeNode(lis[i])
+                root.left = left[l]
+                root.right = right[r]
+                val.append(root)
+    return val
+    
