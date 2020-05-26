dcc155e4689583471d143ea55d1a507d2490d880
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-26 23:41:34 +0530

    Question 101

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -22,3 +22,33 @@ But the following [1,2,2,null,3,null,3] is not:
 Follow up: Solve it both recursively and iteratively.
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root==None:
+            return True
+        return helper(root.left, root.right)
+        
+        
+def helper(left, right):
+    if left==None or right==None:
+        if left==right:
+            return True
+        else:
+            return False
+        
+    if left.val!=right.val:
+        return False
+    
+    l = helper(left.left, right.right)
+    r = helper(left.right, right.left)
+    
+    return l and r
+    
