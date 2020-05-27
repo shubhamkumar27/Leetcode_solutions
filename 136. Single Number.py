f7218752aeecc9a1f1d275f8c93ae61cfcebbbd2
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-27 23:41:46 +0530

    Question 136

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -14,3 +14,10 @@ Example 2:
 Input: [4,1,2,1,2]
 Output: 4
 '''
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            ans ^= i
+        return ans
