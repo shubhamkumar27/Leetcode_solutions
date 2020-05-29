170b834b859ba72bb34caea3db5694c7231c6fb7
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-29 23:42:56 +0530

    Question 137

--- 137. Single Number II.py
+++ 137. Single Number II.py
@@ -15,3 +15,15 @@ Input: [0,1,0,1,0,1,99]
 Output: 99
 '''
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(32):
+            count=0
+            for num in nums:
+                if (num>>i)&1:
+                    count+=1
+            ans |= (count%3)<<i
+        if ans>2**31 -1:
+            return ans-2**32
+        return ans
