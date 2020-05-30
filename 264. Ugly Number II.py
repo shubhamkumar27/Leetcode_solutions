97d176988a1d09bb3638b62107a061ca27be599f
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-30 23:42:10 +0530

    Question 264

--- 264. Ugly Number II.py
+++ 264. Ugly Number II.py
@@ -13,3 +13,28 @@ Note:
 1 is typically treated as an ugly number.
 n does not exceed 1690.
 '''
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        x = 1
+        q2,q3,q5 = [], [], []
+        
+        for i in range(1,n):
+            q2.append(2*x)
+            q3.append(3*x)
+            q5.append(5*x)
+            
+            x = min(q2[0], q3[0], q5[0])
+            
+            if x==q2[0]:
+                q2.pop(0)
+            
+            if x==q3[0]:
+                q3.pop(0)
+                
+            if x==q5[0]:
+                q5.pop(0)
+                
+                
+        return x
+        
