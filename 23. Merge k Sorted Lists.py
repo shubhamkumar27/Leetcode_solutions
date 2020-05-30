333d5d6e541b6ffe9c14563f45c5fa7e2e95b1a1
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-05-30 23:40:59 +0530

    Question 23

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -11,4 +11,28 @@ Input:
 ]
 Output: 1->1->2->3->4->4->5->6
 '''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        minHeap = []
+        for head in lists:
+            while(head):
+                heapq.heappush(minHeap, head.val)
+                head = head.next
+        
+        dummy = ListNode(-1)
+        temp = dummy
+        for i in range(len(minHeap)):
+            node = ListNode(heapq.heappop(minHeap))
+            temp.next = node
+            temp = node
+            
+            
+        return dummy.next
+            
