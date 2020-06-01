17e69739e32c70a84caf57e1cd23d9ce387fc05d
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-01 23:53:29 +0530

    Question 88

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -14,3 +14,19 @@ nums2 = [2,5,6],       n = 3
 Output: [1,2,2,3,5,6]
 '''
 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while(m>0 and n>0):
+            if nums1[m-1]>=nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m-=1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n-=1
+        
+        if n>0:
+            nums1[:n] = nums2[:n]
+
