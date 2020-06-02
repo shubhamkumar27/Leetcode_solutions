49811a1a7eb8df1c2c1476824c83dbfda6a8f316
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-02 23:47:14 +0530

    Question 237

--- 237. Delete Node in a Linked List.py
+++ 237. Delete Node in a Linked List.py
@@ -27,4 +27,23 @@ The given node will not be the tail and it will always be a valid node of the li
 Do not return anything from your function.
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        temp = node
+        while(temp.next):
+            temp.val = temp.next.val
+            prev = temp
+            temp = temp.next
+        prev.next = None
+            
