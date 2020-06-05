63ec67f011d0ea1139fabcdd87652f5a1e4ba746
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-05 23:42:24 +0530

    Question 168

--- 168. Excel Sheet Column Title.py
+++ 168. Excel Sheet Column Title.py
@@ -25,4 +25,16 @@ Input: 701
 Output: "ZY"
 '''
 
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        curr = ''
+        while(n>26):
+            if n%26:
+                curr = chr((n%26)+64)+curr
+                n = n//26
+            else:
+                curr = 'Z'+curr
+                n = (n//26)-1
+        curr = chr(n+64)+curr
+        return curr
 
