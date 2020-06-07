120ed63ca7a866ea2998b5c7d987b85a2e89c974
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-07 23:47:19 +0530

    Question 695

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -21,4 +21,32 @@ Given the above grid, return 0.
 Note: The length of each dimension in the given grid does not exceed 50.
 '''
 
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        
+        dircn = [-1,0,1,0,-1]
+        maxArea = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]==1:
+                    area = 0
+                    grid[i][j]=0
+                    q = deque([(i,j)])
+                    while len(q)>0:
+                        area+=1
+                        r, c = q.pop()
+                        
+                        for k in range(4):
+                            x = r+dircn[k]
+                            y = c+dircn[k+1]
+                            
+                            if row>x>=0 and col>y>=0 and grid[x][y]==1:
+                                grid[x][y]=0
+                                q.append((x,y))
 
+                    maxArea = max(maxArea, area)
+                    
+        return maxArea
