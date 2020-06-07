e04889aacb710b23293ee6978bc5d8150910eedb
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-07 23:45:46 +0530

    Question 406

--- 406. Queue Reconstruction by Height.py
+++ 406. Queue Reconstruction by Height.py
@@ -14,3 +14,12 @@ Output:
 [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
 '''
 
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key = lambda x : (-x[0], x[1]))
+
+        q = []
+        for pair in people:
+            q.insert(pair[1], pair)
+            
+        return q
