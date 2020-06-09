f818c6d7f1308fc1e32d70de3a107945c308afa6
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-09 23:48:35 +0530

    Question 392

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -28,3 +28,17 @@ Constraints:
 Both strings consists only of lowercase characters.
 '''
 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s)==0:
+            return True
+        p = 0
+        for i in range(len(t)):
+            if s[p]==t[i]:
+                p+=1
+                
+            if p==len(s):
+                return True
+        
+        return False
+        
