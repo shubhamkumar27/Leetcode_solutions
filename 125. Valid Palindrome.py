00375ef5f7ee6825331afd6fb459e8869cde384e
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-09 23:40:59 +0530

    Question 125

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -13,4 +13,20 @@ Input: "race a car"
 Output: false
 '''
 
-
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+        
+        while(l<=r):
+            if not s[l].isalnum():
+                l+=1
+            elif not s[r].isalnum():
+                r-=1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                else:
+                    l+=1
+                    r-=1
+        return True
