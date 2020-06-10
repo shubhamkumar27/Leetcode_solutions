b228caf4b1b54fcc788d147932d935f95a587869
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-10 23:58:17 +0530

    Question 200

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -32,3 +32,31 @@ Given the following tree [1,2,2,3,3,null,null,4,4]:
 Return false.
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        val = recursive(root)
+        
+        if val==-1:
+            return False
+        else:
+            return True
+    
+def recursive(root):
+    if root==None:
+        return 0
+    
+    l = recursive(root.left)
+    r = recursive(root.right)
+    
+    if l==-1 or r==-1 or abs(l-r)>1:
+        return -1
+    
+    return 1+max(l,r)
+    
