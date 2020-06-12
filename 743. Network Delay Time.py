a906577da4fa6a525c79e446a5617ed21800cdd0
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-12 23:54:09 +0530

    Question 743

--- 743. Network Delay Time.py
+++ 743. Network Delay Time.py
@@ -23,3 +23,33 @@ The length of times will be in the range [1, 6000].
 All edges times[i] = (u, v, w) will have 1 <= u, v <= N and 0 <= w <= 100.
 '''
 
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        graph = [[] for i in range(N+1)]
+        
+        for edge in times:
+            graph[edge[0]].append([edge[1],edge[2]])
+            
+        distance = [2**31-1]*(N+1)
+        distance[K] = 0
+        
+        minheap = []
+        heapq.heappush(minheap, (0,K))
+        
+        while(len(minheap)):
+            v,u = heapq.heappop(minheap)
+            
+            for neighbor in graph[u]:
+                node, dis = neighbor[0], neighbor[1]
+                
+                if dis+v<distance[node]:
+                    heapq.heappush(minheap, (dis+v,node))
+                    distance[node] = dis+v
+                    
+        d = max(distance[1:])
+        if d==2**31-1:
+            return -1
+        else:
+            return d
+        
