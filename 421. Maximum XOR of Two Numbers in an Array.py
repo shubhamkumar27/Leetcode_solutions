3a14ca665f6379913d4d1a87659009898a365f5e
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-14 23:58:54 +0530

    Question 421

--- 421. Maximum XOR of Two Numbers in an Array.py
+++ 421. Maximum XOR of Two Numbers in an Array.py
@@ -14,4 +14,47 @@ Output: 28
 Explanation: The maximum result is 5 ^ 25 = 28.
 '''
 
+class TrieNode:
+    def __init__(self):
+        self.one = None
+        self.zero = None
 
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        root = TrieNode()
+        
+        for num in nums:
+            temp = root
+            
+            for i in range(31, -1, -1):
+                bit = (num>>i)&1
+                
+                if bit:
+                    if temp.one==None:
+                        temp.one = TrieNode()
+                    temp = temp.one
+                else:
+                    if temp.zero==None:
+                        temp.zero = TrieNode()
+                    temp = temp.zero
+        
+        maxans = 0
+        for num in nums:
+            temp = root
+            ans = 0
+            for i in range(31,-1,-1):
+                bit = (num>>i)&1
+                
+                if bit and temp.zero:
+                    ans += (1<<i)
+                    temp = temp.zero
+                elif bit==0 and temp.one:
+                    ans += (1<<i)
+                    temp = temp.one
+                else:
+                    temp = temp.one or temp.zero
+            
+            maxans = max(maxans, ans)
+            
+        return maxans
+                    
