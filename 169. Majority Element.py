259740a1ffc3526e4b5b2a9583325fd6333761f5
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-22 23:58:12 +0530

    Question 169

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,5 +1,6 @@
 '''
-Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+Given an array of size n, find the majority element. The majority element is the element that 
+appears more than ⌊ n/2 ⌋ times.
 
 You may assume that the array is non-empty and the majority element always exist in the array.
 
@@ -12,3 +13,11 @@ Example 2:
 Input: [2,2,1,1,1,2,2]
 Output: 2
 '''
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0)+1
+            if hashmap[num]==(len(nums)//2)+1:
+                return num
