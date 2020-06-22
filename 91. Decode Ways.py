01c5fa5273d08d11f3d12b6df90bfae005a0e70b
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-22 23:59:26 +0530

    Question 91

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -18,3 +18,24 @@ Input: "226"
 Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 '''
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # global answer
+        # answer = 0
+        # counter(s, 0)
+        # return answer
+        
+        dp = [0]*(len(s)+1)
+        dp[0] = 1
+        if s[0]!='0':
+            dp[1] = 1
+            
+        for i in range(1, len(s)):
+            if s[i]!='0':
+                dp[i+1] += dp[i]
+
+            if 10<=int(s[i-1:i+1])<=26:
+                dp[i+1] += dp[i-1]
+       
+        return dp[-1]
