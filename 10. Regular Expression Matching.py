ef8500e00a3a7d9d76167404ce5590d0e31f77ab
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2020-06-23 23:55:50 +0530

    Question 10

--- 10. Regular Expression Matching.py
+++ 10. Regular Expression Matching.py
@@ -44,3 +44,31 @@ s = "mississippi"
 p = "mis*is*p*."
 Output: false
 '''
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m = len(s)
+        n = len(p)
+        dp = [[False for i in range(n+1)] for j in range(m+1)]
+        
+        dp[0][0] = True
+        
+        for i in range(2,n+1):
+            if p[i-1]=='*':
+                dp[0][i] = dp[0][i-2]
+                
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==p[j-1] or p[j-1]=='.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    op1 = dp[i][j-2]
+                    if p[j-2]==s[i-1] or p[j-2]=='.':
+                        op2 = dp[i-1][j]
+                    else:
+                        op2 = False
+                    dp[i][j] = op1 or op2
+                else:
+                    dp[i][j] = False
+         
+        return dp[m][n]
